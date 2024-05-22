05d91d2f89b942337e2c5f56a021d40c3a5fb643
xyvie lyons <xyvielyons@gmail.com>
2024-05-22 23:27:50 +0300

    final touches (responsiveness)

--- components/Home.tsx
+++ components/Home.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
   return (
     <div className="Home px-4 py-4" id="Home">
         <div className='maincontainer1 flex flex-col md:flex-row md:gap-4 xl:gap-6'>
-            <div className="container1 mb-6 md:p-6 md:space-y-6">
+            <div className="container1 mb-6 md:p-6 md:space-y-6 md:mt-[24px] xl:mt-[48px]">
                     <div className="mb-4 ">
                         <p className='text-coolGray600 text-[20px] font-[500]'>Hello 👋</p>
                         <p className='text-coolGray600 text-[20px] font-[500] mb-2'>I’m Xyvie Lyons</p>
@@ -26,12 +26,12 @@ export default function HomePage() {
                         <a href="https://www.linkedin.com/in/xyvie-lyons-a8873820a" target='_blank'><Button isIconOnly radius="full" className="w-[48px] h-[48px]"><FaLinkedin className='w-[24px] h-[24px] text-coolGray800'></FaLinkedin></Button></a>
                     </div>
 
-                    <div className="flex flex-row space-x-4">
+                    <div className="flex flex-row space-x-2">
                         <div className="">
-                            <a href="https://drive.google.com/file/d/1ys4Bae_we6M-ZoFkpzYo2zOoXsvkv-qr/view?usp=sharing" target='_blank' ><Button  className='bg-accentColor text-white p-4' radius='sm' size='sm' startContent={<IoDocument className='w-[24px] h-[24px]'></IoDocument>}>Download CV</Button></a>
+                            <a href="https://drive.google.com/file/d/1ys4Bae_we6M-ZoFkpzYo2zOoXsvkv-qr/view?usp=sharing" target='_blank' ><Button  className='bg-accentColor text-white p-2' radius='sm' startContent={<IoDocument className='w-[24px] h-[24px]'></IoDocument>}>Download CV</Button></a>
                         </div>
                         <div className="">
-                            <a href="#Portfolio"><Button className='p-4' radius='sm' variant='bordered' size='sm' startContent={<FaLocationArrow className='w-[24px] h-[24px]'/>}>View Portfolio</Button></a>
+                            <a href="#Portfolio"><Button className='p-2' radius='sm' variant='bordered' startContent={<FaLocationArrow className='w-[24px] h-[24px]'/>}>View Portfolio</Button></a>
                         </div>
                     </div>
 
@@ -40,7 +40,7 @@ export default function HomePage() {
             </div>
 
             <div className="container2 mb-6">
-                <div className="max-w-[600px]">
+                <div className="max-w-[500px]">
                     <Image className='' src="/xyvie lyons wireframing and development kit_page-0001.jpg" alt='profile' width={1200} height={768} ></Image>
 
                 </div>
